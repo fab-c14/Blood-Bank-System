ef67a1c2f0dcc4fcdf2c07348765db802efea11c
Faisal Bhat <plesim18@gmail.com>
2024-03-23 12:58:45 +0000

    Login Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,17 @@
 // src/App.js
-import React from 'react';
+import React, { useState } from 'react';
 import RegisterForm from './components/Auth/RegisterForm';
 import LoginForm from './components/Auth/Login';
 
+
 function App() {
+    const [route,setRoute] = useState('home')
+
     return (
         <div className="App">
-            <RegisterForm />
-            <LoginForm />
+         {
+          route==='home'?<RegisterForm/>:<LoginForm/>
+         }
         </div>
     );
 }
