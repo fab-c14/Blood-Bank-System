cf12e5a2a271f22aefffdf0a844b62d14d2ae375
Faisal Bhat <plesim18@gmail.com>
2024-03-24 10:31:39 +0000

    Made Some Changes

--- frontend/src/components/Home/LandingPage.js
+++ frontend/src/components/Home/LandingPage.js
@@ -1,11 +1,10 @@
-import BloodDropSvg from "./svgs/svgs";
-
-
-
-const LandingPage = ()=>{
-    <div>    
-        <BloodDropSvg />
-    </div>
+import './LandingPage.css';
+const LandingPage = () => {
+    return (
+        <div className="container-fluid ">    
+            <h1 className="text-center tracking-in-expand">Blood Bank System</h1>
+        </div>
+    );
 }
 
 export default LandingPage;
