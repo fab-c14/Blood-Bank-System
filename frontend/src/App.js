1e92ac1a5a8937201d54d1771d08c548dfd711fd
Faisal Bhat <plesim18@gmail.com>
2024-03-27 09:51:38 +0000

    About pt-2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import 'tachyons';
 import FeaturesSection from './components/Home/FeatureSection';
-
+import About from './components/Home/About';
 const App = () => {
   return (
     <Router>
@@ -23,12 +23,12 @@ const App = () => {
         <Route path="/signin" element={
         <React.Fragment>
           <LoginForm />
-         
+         <About/>
         </React.Fragment>} />
         <Route path="/register" element={
         <React.Fragment>
           <RegisterForm />
-          
+          <About/>
         </React.Fragment>} />
       </Routes>
     </Router>
