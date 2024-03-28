e11f1397aa63267a6b3199a35a41bb5e9bd34382
Faisal Bhat <plesim18@gmail.com>
2024-03-28 10:07:41 +0000

    Added Logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -25,7 +25,7 @@ const App = () => {
 
   return (
     <Router>
-      <Navbar isLoggedIn = {isLoggedIn} />
+      <Navbar isLoggedIn={isLoggedIn} onLogout={handleLogout} />
       <Routes>
         <Route
           path="/"
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,29 +1,46 @@
 import './Nav.css';
-
+import { Link } from 'react-router-dom';
 import blooddonationlogo from './blooddonation.png';
-const Navbar = () => {
+import React from 'react';
+
+const Navbar = ({ isLoggedIn, onLogout }) => {
+    const handleLogout = () => {
+        // Call the logout function provided by the parent component
+        onLogout();
+    };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <a className="navbar-brand" href="#">
+                <Link className="navbar-brand" to="/">
                     <img src={blooddonationlogo} alt="logo" width={"50px"} className='my-2 pa2'/>
-                    Blood Bank System</a>
+                    Blood Bank System
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                            <Link className="nav-link" to="/">Home</Link>
                         </li>
-                    
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Donate
                             </a>
                             <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a className="dropdown-item" href="/signin">Login</a></li>
-                                <li><a className="dropdown-item" href="/register">Sign Up</a></li>
+                                {isLoggedIn ? (
+                                    <React.Fragment>
+                                        <li><Link className="dropdown-item" to="/profile">Profile</Link></li>
+                                        <li><Link className="dropdown-item" to="/donation">Donate</Link></li>
+                                        <li><button className="dropdown-item" onClick={handleLogout}>Logout</button></li>
+                                    </React.Fragment>
+                                ) : (
+                                    <React.Fragment>
+                                        <li><Link className="dropdown-item" to="/signin">Login</Link></li>
+                                        <li><Link className="dropdown-item" to="/register">Sign Up</Link></li>
+                                    </React.Fragment>
+                                )}
                             </ul>
                         </li>
                     </ul>
