414b85747bad550b4a64cab50201f96f0b4b8735
Faisal Bhat <plesim18@gmail.com>
2024-04-08 09:37:49 +0000

    changed overall footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,28 +1,33 @@
 import React from 'react';
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+import './Footer.css'; // Import CSS file for footer styles
 
 const Footer = () => {
     return (
-        <footer className="bg-dark text-light py-4" id='contact'>
+        <footer className="bg-dark text-light py-4">
             <div className="container">
                 <div className="row">
                     <div className="col-md-6">
-                        <h5 className="text-primary">Contact Us</h5>
-                        <p>123 Main Street, Cityville, ABC 12345</p>
-                        <p>Email: info@bloodbanksystem.com</p>
-                        <p>Phone: 123-456-7890</p>
+                        <div className="footer-box">
+                            <h5 className="text-primary">Contact Us</h5>
+                            <p className="footer-text">Blood Bank System connects donors with hospitals in need of blood. If you have any inquiries or suggestions, feel free to reach out to us via email.</p>
+                            <p><a href="mailto:info@bloodbanksystem.com" className="footer-link">info@bloodbanksystem.com</a></p>
+                        </div>
                     </div>
                     <div className="col-md-6">
-                        <h5 className="text-primary">Quick Links</h5>
-                        <ul className="list-unstyled">
-                            <li><a href="#home" className="text-light">Home</a></li>
-                            <li><a href="#about" className="text-light">About</a></li>
-                            <li><a href="#about" className="text-light">Contact</a></li>
-                        </ul>
+                        <div className="footer-box">
+                            <h5 className="text-primary">Stay Connected</h5>
+                            <div className="social-icons">
+                                <a href="#" className="footer-icon-link"><FaFacebookF /></a>
+                                <a href="#" className="footer-icon-link"><FaTwitter /></a>
+                                <a href="#" className="footer-icon-link"><FaInstagram /></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <hr className="bg-light mt-4 mb-3" />
                 <div className="text-center">
-                    <p className="text-muted">&copy; 2024 Blood Bank System. All rights reserved.</p>
+                    <p className=" white">&copy; 2024 Blood Bank System. All rights reserved.</p>
                 </div>
             </div>
         </footer>
